74884230f294e8e39771b833f65c8d849984a0ef
Christian Murphy <christian.murphy.42@gmail.com>
2020-05-17 12:18:24 -0700

    docs: add remark math example

--- stories/remark-component.stories.tsx
+++ stories/remark-component.stories.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { text } from '@storybook/addon-knobs';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import 'katex/dist/katex.min.css';
+
 import { Remark } from '../src';
 
 export default {
@@ -7,16 +11,30 @@ export default {
   component: Remark,
 };
 
-export const Default = () =>
-  // prettier-ignore
+export const Default = () => (
   <Remark>
-    {text("content", `# header
+    {text(
+      'content',
+      `# header
 
 1. ordered
 2. list
 
 * unordered
-* list
+* list`
+    )}
+  </Remark>
+);
 
-`)}
-  </Remark>;
+export const Math = () => (
+  <Remark remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+    {text(
+      'content',
+      `Lift($L$) can be determined by Lift Coefficient ($C_L$) like the following equation.
+
+$$
+L = \\frac{1}{2} \\rho v^2 S C_L
+$$`
+    )}
+  </Remark>
+);
