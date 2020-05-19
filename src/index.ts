b47bde976d1dd653a4bcec258f1f762159a98c8a
Christian Murphy <christian.murphy.42@gmail.com>
2020-05-19 08:24:35 -0700

    feat: upgrade remark rehype and leverage typings

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,14 @@ import {
 } from 'react';
 import unified, { PluggableList } from 'unified';
 import remarkParse, { RemarkParseOptions } from 'remark-parse';
-// @ts-ignore
+import { Options as RemarkRehypeOptions } from 'mdast-util-to-hast';
 import remarkToRehype from 'remark-rehype';
 // @ts-ignore
 import rehypeReact from 'rehype-react';
 
 export interface UseRemarkOptions {
   remarkParseOptions?: Partial<RemarkParseOptions>;
-  // TODO: Pending https://github.com/remarkjs/remark-rehype/pull/13
-  remarkToRehypeOptions?: {};
+  remarkToRehypeOptions?: RemarkRehypeOptions;
   // TODO: Pending
   rehypeReactOptions?: {};
   remarkPlugins?: PluggableList;
